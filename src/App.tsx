8718d1eccd2a2049c611f20152c90f08a29941f1
Yitzhakpro <isako14@gmail.com>
2023-01-28 20:44:30 +0200

    wired up control panel, sort controls hook and sorting container together

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { ControlPanel, SortingContainer } from "./components";
+import useSortControls from "./hooks";
+
 function App() {
-  return <div></div>;
+  const { list, genNewList } = useSortControls();
+
+  return (
+    <>
+      <SortingContainer list={list} />
+      <ControlPanel genNewList={genNewList} />
+    </>
+  );
 }
 
 export default App;
